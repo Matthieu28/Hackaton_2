1f140e37d7af9085e495d9c64723f3e86d0e59b0
Matthieu <Matthieu.Maginot@gmail.com>
2023-01-11 17:08:41 +0100

    pageHome

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,25 +24,33 @@ export default function Home() {
   return (
     <div className="container-home">
       <section>
-        <h2>Vehicle List</h2>
+        <span className="container-title">Vehicle List :</span>
         <div className="makes-grid">
           {vehicle.map((make) => (
             <div className="vehicle-card" key={`make-${make.id}`}>
-              <div
-                style={{
-                  backgroundImage: `url("${make.image}")`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  width: "10em",
-                  height: "7em",
-                  borderRadius: "15px",
-                }}
-              />
-              <div className="title-card">
-                <span className="title-vehicle">
-                  {make.model.toUpperCase()}
-                </span>
+              <div className="vehicle-card-left">
+                <div
+                  style={{
+                    backgroundImage: `url("${make.image}")`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    width: "10em",
+                    height: "7em",
+                    borderRadius: "15px",
+                  }}
+                />
+                <div className="title-card">
+                  <span className="title-vehicle">
+                    {make.model.toUpperCase()}
+                  </span>
+                </div>
+              </div>
+              <div className="vehicle-card-right">
+                <div className="info-vehicle">{make.fiche}</div>
+                <button type="button" className="button-card-info">
+                  Add to cart
+                </button>
               </div>
             </div>
           ))}
