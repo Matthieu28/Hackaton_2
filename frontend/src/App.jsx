bb6266f01950a6e807937e558f57b928d7cb42bb
Enzo BLOTTIN <enzoblottin@gmail.com>
2023-01-11 16:55:24 +0100

    wesh uwu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,11 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/panier" element={<Panier />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           {currentUser.id && <Route path="/makes" element={<MakeList />} />}
+          <Route path="/map" element={<Map />} />
           <Route path="*" element={<p>404 Not Found</p>} />
         </Routes>
       </main>
---
+++ frontend/src/pages/Panier.jsx
@@ -0,0 +1,15 @@
+import PanierCard from "@components/PanierCard";
+import "./Panier.css";
+
+const Panier = () => {
+  return (
+    <div className="panier">
+      <div className="titrePanier">
+        <p>Mon Panier</p>
+      </div>
+      <PanierCard />
+    </div>
+  );
+};
+
+export default Panier;
